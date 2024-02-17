cfa946da9ec3d719e45fbf5408ce0bf5d178fabb
Annhilati <antbe.artis@web.de>
2024-02-17 08:31:42 +0100

    Sync

---
+++ LBD Shaders/shaders2/lib/post/depth_saturation.glsl
@@ -0,0 +1,10 @@
+        float depth = vec4(texture2D(depthtex0, texcoord).r).r
+
+vec4 saturate(vec4 inputColor, float saturation) {
+    vec4 ReturnColor;
+    if (inputColor.r == inputColor.g && inputColor.r == inputColor.b) {
+
+    } else if (inputColor.r > inputColor.g && inputColor.r > inputColor.b) {
+        
+    }
+}
