dcc24c9dbd4859cbe35a48e4ae03d250bf1d9f61
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-02-18 08:56:28 +0100

    Sync

--- LBD Shaders/shaders2/lib/post/color_grading.glsl
+++
@@ -1,8 +0,0 @@
-vec4 sepia(vec4 inputColor) {
-    float average = (inputColor.r + inputColor.g + inputColor.b) / 3.0;
-    return vec4(average + 0.15, average + 0.075, average, inputColor.a);
-}
-
-vec4 wdh_cM21(vec4 inputColor) {
-    return vec4(log(sqrt(pow(inputColor.r, 2) + pow(inputColor.g, 2) + pow(inputColor.b, 2))), 0.0, 0.0, inputColor.a);
-}
