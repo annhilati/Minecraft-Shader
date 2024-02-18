d587b3621a8dbfb8fb5c98434f8f571e5f7f4608
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-02-18 08:57:01 +0100

    Sync

--- lib/post/effects.glsl
+++ lib/post/effects.glsl
@@ -1,17 +1,3 @@
-vec4 grayscale(vec4 inputColor) {
-    float average = (inputColor.r + inputColor.g + inputColor.b) / 3.0;
-    return vec4(vec3(average), inputColor.a);
-}
-
-vec4 sepia(vec4 inputColor) {
-    float average = (inputColor.r + inputColor.g + inputColor.b) / 3.0;
-    return vec4(average + 0.15, average + 0.075, average, inputColor.a);
-}
-
-vec4 invert(vec4 inputColor) {
-    return vec4(1 - inputColor);
-}
-
 vec4 simplify(vec4 inputColor) {
     // Alle sind gleich
     if (inputColor.r == inputColor.g && inputColor.r == inputColor.b) {
